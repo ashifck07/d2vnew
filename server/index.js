cc6789559d33abec7b462c152bf2a0ee7fe67e50
ashifck07 <ashifck.stackup@gmail.com>
2025-02-05 08:10:06 +0530

    require check

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
-require("dotenv").config();
 
 const express = require("express");
+require("dotenv").config();
 const bodyParser = require("body-parser");
 const {checkFirebaseConnection} = require("./config/firebase");
 
